0d1dce82b7464eebd471c3f998df5562fe888583
andrea abed <andrea.abed@yahoo.com>
2020-11-18 09:11:53 -0800

    change home page, change embedding in "all" to match portfolio

--- europe_travel/data.py
+++ europe_travel/data.py
@@ -1,35 +1,41 @@
-def greece():
-    greeting = "Welcome to Greece"
-    embed = "http://127.0.0.1:5000/greece/"
+def wesley_journal():
+    greeting = "Wesley and Naweid's Journal"
+    embed = "https://docs.google.com/document/d/15XZWCyArc6Fv6Ywa5bqmP_nOSDWnGqM-ursyV09_FxM/edit"
     info = {"greeting": greeting, "embed": embed}
     return info
 
-def italy():
-    greeting = "Welcome to Italy"
-    embed = "http://127.0.0.1:5000/italy/"
+def andrea_journal():
+    greeting = "Andrea, Ethan, Diego's Journal"
+    embed = "https://docs.google.com/document/d/1QPwyc1Xuv9RBLs38YVvEXeJLtfcbk_punZXeIJM1wcw/edit"
     info = {"greeting": greeting, "embed": embed}
     return info
 
-def spain():
-    greeting = "Welcome to Spain"
-    embed = "http://127.0.0.1:5000/spain/"
+def flask_projectplan():
+    greeting = "Flask Series Project Plan"
+    embed = "https://docs.google.com/document/d/1VJz3bjcGSNatEY4aVoDGxhfeJRwPQQI6zGf2Ji53uDk/edit"
     info = {"greeting": greeting, "embed": embed}
     return info
 
-def france():
-    greeting = "Welcome to France"
+def flask_project():
+    greeting = "Flask Series Travel Wesbite Project"
     embed = "http://127.0.0.1:5000/france/"
     info = {"greeting": greeting, "embed": embed}
     return info
 
-def germany():
-    greeting = "Welcome to Germany"
-    embed = "http://127.0.0.1:5000/italy/"
+def hello_projectplan():
+    greeting = "Hello Series Project PLan"
+    embed = "https://docs.google.com/document/d/1n8XvhfdLlLcQkmsrgegQgb6QPyclnFSn_wVakUGTJAo/edit"
+    info = {"greeting": greeting, "embed": embed}
+    return info
+
+def hello_project():
+    greeting = "Hello Series Shop Game Project"
+    embed = "https://repl.it/@AndreaAbed/copyofnewbiecodershop?lite=true"
     info = {"greeting": greeting, "embed": embed}
     return info
 
 def alldata():
-    return [greece(), italy(), spain(), france(), germany()]
+    return [wesley_journal(), andrea_journal(), flask_projectplan(), flask_project(), hello_projectplan(), hello_project()]
 
 #Data "setup" for Projects
 #next step would be to extract project data from a database
